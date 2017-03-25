4adb0ed2356406cf801a72e63c5032a4b2572e29
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-03-25 13:42:01 +0100

    implemented mock services

---
+++ NancyFxCSharpCompanies/Services/MockServices/DepartmentMockService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NancyFxCSharpCompanies
+{
+	public class DepartmentMockService : IDepartmentService
+	{
+		private ICollection<Department> departments = new List<Department>();
+
+		public DepartmentMockService()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				departments.Add (new Department (){ 
+					Id = i,
+					Name = "Department" + i
+				});
+			}
+		}
+
+		public async Task<ICollection<Department>> GetDepartments()
+		{
+			return departments;
+		}
+
+		public async Task<Department> GetDepartment(int id)
+		{
+			return departments.Where (c => c.Id == id).FirstOrDefault ();
+		}
+	}
+}
+
---
+++ NancyFxCSharpCompanies/Services/MockServices/EmployeeMockService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NancyFxCSharpCompanies
+{
+	public class EmployeeMockService : IEmployeeService
+	{
+		private ICollection<Employee> employees = new List<Employee>();
+
+		public EmployeeMockService()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				employees.Add (new Employee (){ 
+					Id = i,
+					FirstName = "John" + i,
+					SecondName = "Doe" + i
+				});
+			}
+		}
+
+		public async Task<ICollection<Employee>> GetEmployees()
+		{
+			return employees;
+		}
+
+		public async Task<Employee> GetEmployee(int id)
+		{
+			return employees.Where (c => c.Id == id).FirstOrDefault ();
+		}
+	}
+}
+
